4774ac7f75e912e3cf4a7c25902361b1595b0785
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-04-21 04:17:35 +0200

    adding the orders route

--- server.js
+++ server.js
@@ -1,33 +1,68 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const product = require('./models/productModel')
+const product = require('./models/productModel');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-mongoose.connect('mongodb://localhost/react-shopping-cart', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
-    console.log('connection is good')
+mongoose
+  .connect('mongodb://localhost/react-shopping-cart', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connection is good');
+  });
+
+const order = mongoose.model(
+  'order',
+  new mongoose.Schema(
+    {
+      name: String,
+      email: String,
+      total: Number,
+      cart: [{ quantity: Number, title: String }],
+    },
+    {
+      timestamps: true,
+    },
+  ),
+);
+
+app.get('/api/orders', async (req, res) => {
+  const orders = await order.find();
+  res.send(orders);
 });
 
+app.post('/api/orders', async (req, res) => {
+  const newOrder = new order(req.body);
+  const savedOrder = await newOrder.save();
+  res.send(savedOrder);
+});
 
+app.delete('/api/orders/:id', async (req, res) => {
+  const deletedOrder = await order.findByIdAndDelete(req.params.id);
+  res.send(deletedOrder);
+});
 
 app.get('/api/products', async (req, res) => {
-    const products = await product.find()
-    res.send(products)
+  const products = await product.find();
+  res.send(products);
 });
 
-app.post('/api/products', async (req, res)=>{
-    const newProduct = new product(req.body);
-    const savedProduct = await newProduct.save();
-    res.send(savedProduct)
-})
+app.post('/api/products', async (req, res) => {
+  const newProduct = new product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
 
-app.delete('/api/products/:id', async (req, res)=>{
-    
-    const deletedProduct = await product.findByIdAndDelete(req.params.id);
-    res.send(deletedProduct);
-})
+app.delete('/api/products/:id', async (req, res) => {
+  const deletedProduct = await product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
 app.listen(port, () => console.log(`listening on port ${port}!`));
